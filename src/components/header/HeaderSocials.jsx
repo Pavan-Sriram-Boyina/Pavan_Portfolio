48709febf71b90645ec114bae0f078567f212936
pavan <pavansriram1523@gmail.com>
2024-05-24 13:47:54 +0530

    removed BsMedium

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
-import {BsMedium} from 'react-icons/bs'
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
 
 
 const HeaderSocials = () => {
-    
-    return(
-            <div className="header__socials">
-                <a href="https://www.linkedin.com/in/adithya-venkata-pavan-sriram-boyina-558377259/" target="_blank" ><BsLinkedin/></a>
-                <a href="https://github.com/Pavan-Sriram-Boyina" target="_blank" ><BsGithub/></a>
-                
-            </div>
-    )
-}
+  return (
+    <div className="header__socials">
+      <a
+        href="https://www.linkedin.com/in/adithya-venkata-pavan-sriram-boyina-558377259/"
+        target="_blank"
+      >
+        <BsLinkedin />
+      </a>
+      <a href="https://github.com/Pavan-Sriram-Boyina" target="_blank">
+        <BsGithub />
+      </a>
+    </div>
+  );
+};
 
-export default HeaderSocials
+export default HeaderSocials;
